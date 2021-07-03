6d08c504cd72f978d43b194c3eb55434df0e158a
nathan lucena <nathan.lucena89@gmail.com>
2021-07-03 10:51:44 -0300

    API consumptionn working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import { useState } from "react";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+import { Home } from "./pages/Home/Home";
+import { Profile } from "./pages/Profile/Profile";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Folows } from "./components/Folows/Folows";
+
+// type ObgType = {
+//   name: string;
+//   url: string;
+// };
+
+// type Character = {
+//   id: number;
+//   name: string;
+//   avatar: string;
+//   status: string;
+//   gender: string;
+//   specie: string;
+//   location: ObgType;
+//   origin: ObgType;
+//   episode: string[];
+// };
 
 function App() {
+  const [searchName, setSearchName] = useState<string>("teste");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar />
+      <div className="a">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route exact path="/:character" component={Profile} />
+          </Switch>
+        </BrowserRouter>
+        <Folows />
+      </div>
     </div>
   );
 }
